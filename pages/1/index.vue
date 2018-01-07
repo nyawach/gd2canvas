e2c727256b78e6886ca19c6b010af6ffd618d40e
himeno-yusuke <himeno-yusuke@kayac.com>
2018-01-08 00:32:02 +0900

    四角を表示した

--- pages/1/index.vue
+++ pages/1/index.vue
@@ -1,13 +1,35 @@
 <template lang="pug">
 section.container
   h1.title テスト
-  canvas#app
+  canvas#canvas(ref="canvas")
 </template>
 
 <script>
+import * as PIXI from 'pixi.js'
+
 export default {
   name: 'index-1',
+  data: () => ({
+    app: null,
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }),
   mounted () {
+    this.app = new PIXI.Application(
+      this.width,
+      this.height,
+      {
+        view: this.$refs.canvas,
+      },
+    )
+    const g = new PIXI.Graphics()
+    g.beginFill(0xfffff, 1)
+    g.lineStyle(10, 0x00ff00, 1)
+    g.drawRect(100, 100, 50, 50)
+    g.endFill()
+    this.app.stage.addChild(g)
+    this.app.render(this.app.stage)
+    this.app.start()
   },
 }
 </script>
